4798c2c26fbf18fb4b1541fcaf6c3dca61e7b849
Hareth2000 <harethj18@gmail.com>
2025-04-07 11:03:48 +0300

    connect Db done

--- next-pro/src/lib/db.js
+++ next-pro/src/lib/db.js
@@ -1,10 +1,10 @@
-// lib/dbConnect.js
-import mongoose from 'mongoose';
+// lib/mongodb.js
+import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+  throw new Error("⚠️ MONGODB_URI is not defined in .env.local");
 }
 
 let cached = global.mongoose;
@@ -13,19 +13,20 @@ if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
-  }
+export async function connectToDB() {
+  if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: "test-connection",
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    }).then((mongoose) => mongoose);
+    }).then((mongoose) => {
+      console.log("✅ Connected to MongoDB!");
+      return mongoose;
+    });
   }
+
   cached.conn = await cached.promise;
   return cached.conn;
 }
-
-export default dbConnect;

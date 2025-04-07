9981067ec63af1016269018fae1ee2186a45382f
jenan-faraj <jenan.faraj4@gmail.com>
2025-04-08 00:27:16 +0300

    add articles page

--- next-pro/src/app/components/navbar.jsx
+++ next-pro/src/app/components/navbar.jsx
@@ -12,6 +12,7 @@ const navLinks = [
   { name: "User Profile", path: "/userProfile" },
   { name: "Admin Dashboard", path: "/adminDashbord" },
   { name: "Payment", path: "/Payment" },
+  { name: "Articles", path: "/articles" },
 ];
 
 export default function Navbar() {

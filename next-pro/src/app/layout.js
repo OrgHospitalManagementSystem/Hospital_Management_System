a3f523ee561dded7c2d0c8fc2b7cfb3bf4d34848
jenan-faraj <jenan.faraj4@gmail.com>
2025-04-11 14:55:51 +0300

    add doctors page

--- next-pro/src/app/layout.js
+++ next-pro/src/app/layout.js
@@ -1,20 +1,19 @@
 import "./globals.css";
 import Navbar from "./components/navbar";
 import SessionWrapper from "@/app/components/SessionWrapper";
+import Footer from "./components/Footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        
-      </head>
+      <head></head>
       <body>
-      <SessionWrapper>
-
-      <Navbar/>
-      <main>{children}</main>
-      </SessionWrapper>
-
-    </body></html>
+        <SessionWrapper>
+          <Navbar />
+          <main>{children}</main>
+        </SessionWrapper>
+        <Footer />
+      </body>
+    </html>
   );
 }

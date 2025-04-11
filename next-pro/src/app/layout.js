699a9d79d51498bd3e967248632363d3ac22bc35
jenan-faraj <jenan.faraj4@gmail.com>
2025-04-11 21:07:46 +0300

    add video call

--- next-pro/src/app/layout.js
+++ next-pro/src/app/layout.js
@@ -3,6 +3,11 @@ import Navbar from "./components/navbar";
 import SessionWrapper from "@/app/components/SessionWrapper";
 import Footer from "./components/Footer";
 
+export const metadata = {
+  title: 'تطبيق مكالمات الفيديو',
+  description: 'تطبيق مكالمات فيديو باستخدام Next.js و WebRTC',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
